bf5c98837001ee543663a0e1705f93138357af14
msharif34 <msharif34@yahoo.com>
2015-06-21 18:56:47 -0700

    wrote the getWinner code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -37,13 +37,42 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
-}
+     var winner;
+     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+     /* YOUR CODE HERE */
++    if (playerMove == "rock" && computerMove == "scissors") {
++        winner = "player"
++        console.log("Player wins!");
++    }
++    else if (playerMove == "scissors" && computerMove == "rock") {
++        winner = "computer"
++        console.log("Computer wins!");
++    }
++    else if (playerMove == "rock" && computerMove == "paper") {
++        winner = "computer"
++        console.log("Computer wins!");
++    }
++    else if (playerMove == "paper" && computerMove == "rock") {
++        winner = "player"
++        console.log("Player wins!");
++    }
++    else if (playerMove == "paper" && computerMove == "scissors") {
++        winner = "computer"
++        console.log("Computer wins!");
++    }
++    else if (playerMove == "scissors" && computerMove == "paper") {
++        winner = "player"
++        console.log("Player wins!");
++    }
++    else {
++        winner = "tie"
++        console.log("Tie!");
++    }
++
+     return winner;
+ }
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
